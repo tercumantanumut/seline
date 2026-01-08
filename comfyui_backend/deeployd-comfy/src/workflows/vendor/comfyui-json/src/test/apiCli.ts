3c17f4d02d11fcc3f88c61b67fad1561270aa604
Umut Tan <tercumantanumut@gmail.com>
2026-01-08 23:12:39 +0300

    fix: Remove vendor directory with Bun-specific TypeScript files

--- comfyui_backend/deeployd-comfy/src/workflows/vendor/comfyui-json/src/test/apiCli.ts
+++
@@ -1,7 +0,0 @@
-import { loadApiJson } from "../fileToWorkflow";
-import { generateDependencyGraph } from "..";
-import x from "../api.json";
-
-generateDependencyGraph({ workflow_api: x }).then((graph) => {
-  console.log(JSON.stringify(graph, null, 2));
-});
